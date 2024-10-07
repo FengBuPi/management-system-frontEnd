b3c6c22f35602edfa8099a27ce55140bb409bd96
疯不皮 <2921744480@qq.com>
2024-10-07 23:36:33 +0800

    style: 代码样式修改

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,22 +1,27 @@
 import React, { ReactNode, useEffect } from 'react'
-import LoginAPI from '@/api/login/Login'
+import LoginAPI from '@/api/login/login'
 import { Link } from 'react-router-dom'
+
+interface User {
+  username: string,
+  password: string
+}
+
 const Login = () => {
+
   async function login(body: object): Promise<any> {
     console.log("发起登录")
     const res = await LoginAPI.login(body)
     console.log("登录结果", res)
   }
+
   async function profile(): Promise<any> {
     for (let i = 0; i < 5; i++) {
       const res = await LoginAPI.profile()
-      console.log(res)
+      // console.log(res)
     }
   }
-  interface User {
-    username: string,
-    password: string
-  }
+
   const user: User = {
     username: "xhf",
     password: "132456"
